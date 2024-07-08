4f609e2ff6ad7c9452dc77f5d658f51c49fda8bc
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-07-08 18:31:26 +0300

    chore: alembic migrations set up

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -5,7 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from db.base import DATABASE_URL, Base§
+
+from db.base import DATABASE_URL, Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
